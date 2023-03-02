8001b1422e620d07a968a8811839fe0c3e655125
Lateef Wasiu Olalekan <olaokunolalekan@gmail.com>
2023-03-02 23:27:09 +0100

    first commit

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Lorem ipsum dolor sit amet.</p>
+      <p>Lorem ipsum dolor sit amet.</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Footer from "@/components/footer";
+import Header from "@/components/header";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <div>Homepage</div>
+      <p>Lorem ipsum dolor sit amet.</p>
+      <p>Lorem ipsum dolor sit amet.</p>
+      <Footer />
+    </>
+  );
+}
